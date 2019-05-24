f8b3d6c2b3ad547acd88967dae2f130410526d49
echo.guan <echo_guan@outlook.com>
2019-05-24 14:26:54 +0800

    Update Count Primes

--- src/com/echo/easy/_204/CountPrimesEeatosthese.java
+++ src/com/echo/easy/_204/CountPrimesEeatosthese.java
@@ -3,6 +3,8 @@ package com.echo.easy._204;
 /**
  * 题目：204. 计数质数（统计所有小于非负整数 n 的质数的数量）
  * 思路：厄拉多塞筛法（Sieve of Eeatosthese）
+ * <p>
+ * 思路详解：https://echoguan.coding.me/2019/05/24/Count-Primes-Sieve-of-Eeatosthese/
  *
  * @author echo.guan
  * @date 2019/05/23
@@ -15,15 +17,21 @@ public class CountPrimesEeatosthese {
         }
 
         Boolean[] num = new Boolean[n];
-        int count = 0;
 
-        for (int i = 2; i < n; i++) {
+        // 排除非质数，置为 false
+        for (int i = 2; i * i < n; i++) {
             if (num[i] == null) {
-                count++;
-
                 for (int j = 2; j * i < n; j++) {
                     num[j * i] = false;
                 }
+            }
+        }
+
+        // 计算数量
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (num[i] == null) {
+                count++;
             }
         }
 
