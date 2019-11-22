711ae087b6c5a5a2dc523b440e6a87486aa0d0d5
echo.guan <echo_guan@outlook.com>
2019-11-22 10:48:30 +0800

    [104] Max depth of binary tree.

--- src/com/echo/leetcode/easy/_104/MaxDepth.java
+++ src/com/echo/leetcode/easy/_104/MaxDepth.java
@@ -1,14 +1,23 @@
 package com.echo.leetcode.easy._104;
 
-import javax.swing.tree.TreeNode;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class MaxDepth {
 
     public int maxDepth(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return 0;
         } else {
-            int left = maxDepth(root.)
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return 1 + Math.max(left, right);
         }
     }
 }
