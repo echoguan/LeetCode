3652557b24440b0977bb23f02d05c98576f43206
echo.guan <echo_guan@outlook.com>
2019-11-22 17:10:24 +0800

    [136] Single Number using sort: WIP

--- src/com/echo/Main.java
+++ src/com/echo/Main.java
@@ -1,9 +1,11 @@
 package com.echo;
 
+import com.echo.leetcode.easy._136.SingleNumberUsingSort;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] input = new int[]{1, 1, 2, 0, 9, 3, -12, 7, 8, 3, 4, 65, -22};
-//        System.out.println(Arrays.toString(new SortArray().sortArray(input)));
+        int[] input = new int[]{4,1,2,1,2};
+        new SingleNumberUsingSort().singleNumber(input);
     }
 }
