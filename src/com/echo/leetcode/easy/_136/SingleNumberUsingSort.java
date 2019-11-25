69414587647cfba8aecc97a80ddb295c6386c108
echo.guan <echo_guan@outlook.com>
2019-11-25 10:34:37 +0800

    [136] Single Number using sort.

--- src/com/echo/leetcode/easy/_136/SingleNumberUsingSort.java
+++ src/com/echo/leetcode/easy/_136/SingleNumberUsingSort.java
@@ -2,10 +2,19 @@ package com.echo.leetcode.easy._136;
 
 import java.util.Arrays;
 
+/**
+ * Find single number using sort.
+ * <p>
+ * Tips:
+ * - Time complexity: O(n log(n))
+ * - Space complexity: O(1)
+ *
+ * @author echo.guan
+ */
 public class SingleNumberUsingSort {
 
     public int singleNumber(int[] nums) {
-        //
+        // JDK using DualPivotQuicksort, and it offers O(n log(n)) performance
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 1; i += 2) {
