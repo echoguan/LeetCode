c7e71f6bef0110e6400eae7c992202c72b3da037
echo.guan <echo_guan@outlook.com>
2019-12-26 11:12:52 +0800

    [206] - Reverse Linked List using iterative.

---
+++ src/com/echo/leetcode/easy/_206/ListNode.java
@@ -0,0 +1,10 @@
+package com.echo.leetcode.easy._206;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/echo/leetcode/easy/_206/ReverseLinkedList.java
@@ -0,0 +1,23 @@
+package com.echo.leetcode.easy._206;
+
+/**
+ * Reverse Linked List using iterative
+ *
+ * - Time complexity: O(n)
+ * - Space complexity: 1
+ *
+ * @author echo.guan
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+
+        while (head != null) {
+            ListNode nextTemp = head.next;
+            head.next = pre;
+            pre = head;
+            head = nextTemp;
+        }
+        return pre;
+    }
+}
